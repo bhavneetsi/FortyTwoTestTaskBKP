3f1025d9e317ff3ff748966f0230701fda1d1c3d
tk <bhavneetsi@gmail.com>
2018-09-01 18:12:43 +0530

    models.py testcase and fixtures file

---
+++ apps/t1_contact/tests/test_models.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from t1_contact.models import Contact 
+
+
+
+class ContactModelTestCase(TestCase):
+
+	
+	def setUp(self):
+
+		self.contact = Contact.objects.create(
+
+			name='Bhavneet',
+			surname='Singh',
+			dateofbirth='1983-05-01',
+			bio='',
+			email='bhavneetsi@gmail.com',
+			jabber='bhavneetsi@42cc.co',
+			skype='bhavneet.si',
+			othercontacts=''
+			
+		
+		)
+	
+	
+	def test_contact_basic(self):
+
+		self.assertEqual(self.contact.name,'Bhavneet')
+
